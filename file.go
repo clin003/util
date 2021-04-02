391f3ecad39ba3104eee597216fb67a98d351604
白菜林 <clin003@vip.qq.com>
2021-04-02 16:25:30 +0800

    file func

---
+++ file.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"io/ioutil"
+	"os"
+	// "github.com/sirupsen/logrus"
+	// "gitee.com/lyhuilin/log"
+)
+
+// ReadFile 读取文件
+// 读取失败返回 nil
+func ReadFile(path string) (retByte []byte, err error) {
+	retByte, err := ioutil.ReadFile(path)
+	if err != nil {
+		// logrus.WithError(err).WithField("util", "ReadFile").Errorf("unable to read '%s'", path)
+		// log.Errorf(err, "unable to read '%s'", path)
+		return //nil, err
+	}
+	return //bytes
+}
+
+// FileExist 判断文件是否存在
+func IsFileExist(path string) (bool, error) {
+	return IsExists(path)
+}
