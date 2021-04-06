264651c20661dcc45901dbcdf9bb7396a5d8ad13
白菜林 <clin003@vip.qq.com>
2021-04-06 20:20:58 +0800

    time

--- time.go
+++ time.go
@@ -8,3 +8,15 @@ import (
 func TimeStampNow() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
 }
+
+// fmt.Println(time.Now().Unix())
+// fmt.Println(time.Now().UnixNano() / 1e6)
+// 13位时间戳 UnixMilli
+// isSecond=true 返回10位时间戳。isSecond=false 返回13位时间戳
+func TimeStampNowEx(isSecond bool) int64 {
+	if isSecond {
+		return time.Now().Unix()
+	} else {
+		return time.Now().UnixNano() / int64(time.Millisecond)
+	}
+}
