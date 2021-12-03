64b3fd8a518baee81f75ac2826aa53190c3b09e5
白菜林 <noreply@3ae.cn>
2021-12-04 00:25:40 +0800

    debug

--- time_sleep.go
+++ time_sleep.go
@@ -18,3 +18,17 @@ func SleepRandTimeSecond(n int64) {
 	randInt := rand.Int63n(n)
 	time.Sleep(time.Duration(randInt) * 2 * time.Second)
 }
+
+// 休眠随机时间（分钟）2_n*2
+func RandTimeMinute(n int64) time.Duration {
+	rand.Seed(time.Now().Unix())
+	randInt := rand.Int63n(n)
+	return time.Duration(randInt) * 2 * time.Minute
+}
+
+// 休眠随机时间（秒） 2_n*2
+func RandTimeSecond(n int64) time.Duration {
+	rand.Seed(time.Now().Unix())
+	randInt := rand.Int63n(n)
+	return time.Duration(randInt) * 2 * time.Second
+}
