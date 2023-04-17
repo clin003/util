91eaa47d5cb7a0c8e6f10be66d81d644e298701a
白菜林 <noreply@3ae.cn>
2023-04-18 00:55:30 +0800

    debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+VERSION=0.1.0
+APPNAME=util
+
 git add .
 git commit -m "debug"
 #git remote rename origin gitee
@@ -8,3 +11,7 @@ git push -u gitee main
 #git remote add github git@github.com:clin003/util.git
 #git branch -M main
 git push -u github main
+
+git tag "v${VERSION}"
+git push --tags -u github main
+git push --tags -u gitee main
