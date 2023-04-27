adef8799fedee05b3f3ab04f92ffbc1d1fe76bdb
白菜林 <noreply@3ae.cn>
2023-04-27 12:47:16 +0800

    debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.3
+VERSION=0.1.5
 APPNAME=util
 
 git add .
---
+++ counter.go
@@ -0,0 +1,63 @@
+package util
+
+import (
+	"fmt"
+	"sync"
+)
+
+var Counter *BCounter
+
+func init() {
+	Counter = new(BCounter)
+}
+
+type BCounter struct {
+	lock    sync.Mutex
+	counter int64
+}
+
+func (c *BCounter) Inc() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	value := c.counter
+	value++
+	if value > 100000000 {
+		value = 0
+	}
+	c.counter = value
+	// if c.lock.TryLock() {
+	// 	c.lock.Lock()
+	// 	defer c.lock.Unlock()
+	// 	value := c.counter
+	// 	value++
+	// 	c.counter = value
+	// } else {
+	// 	fmt.Println(".", c.counter)
+	// }
+}
+func (c *BCounter) IsNice() bool {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	value := c.counter
+	if (value % 1000) == 0 {
+		return true
+	} else {
+		return false
+	}
+
+	// if c.lock.TryLock() {
+	// 	c.lock.Lock()
+	// 	defer c.lock.Unlock()
+
+	// 	value := c.counter
+	// 	if (value % 1000) == 0 {
+	// 		return true
+	// 	} else {
+	// 		return false
+	// 	}
+	// 	// return ((value % 1000) == 0)
+	// } else {
+	// 	return false
+	// }
+}
