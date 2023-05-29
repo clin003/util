4e53a6da1d6c9b88e8656a21b828bb6c85340d25
白菜林 <noreply@3ae.cn>
2023-05-29 11:49:26 +0800

    debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.1.18
+VERSION=0.1.19
 APPNAME=util
 
 git add .
--- get_text_cleaner_hash.go
+++ get_text_cleaner_hash.go
@@ -20,19 +20,49 @@ func GetTextCleaner(textStr string) (retText, retHash string) {
 	for _, v := range textTmp {
 		vcr := v
 		vc := string(v)
-		// switch {
-		// case isChinese.MatchString(vc):
-		// case unicode.IsLetter(vcr):
-		// default:
-		// 	textTmp = strings.ReplaceAll(textTmp, vc, "")
-		// }
 		if isChinese.MatchString(vc) || unicode.IsLetter(vcr) {
 		} else {
 			textTmp = strings.ReplaceAll(textTmp, vc, "")
 		}
 	}
+	// 清理变异字
 	textTmp = strings.ReplaceAll(textTmp, "元", "")
 	textTmp = strings.ReplaceAll(textTmp, "亓", "")
+	textTmp = strings.ReplaceAll(textTmp, "劵", "卷")
+	textTmp = strings.ReplaceAll(textTmp, "拎", "领")
+	textTmp = strings.ReplaceAll(textTmp, "単", "单")
+	textTmp = strings.ReplaceAll(textTmp, "吤", "价")
+	textTmp = strings.ReplaceAll(textTmp, "菅", "营")
+	textTmp = strings.ReplaceAll(textTmp, "咐", "付")
+	textTmp = strings.ReplaceAll(textTmp, "歀", "款")
+	textTmp = strings.ReplaceAll(textTmp, "啇", "商")
+
+	textTmp = strings.ReplaceAll(textTmp, "便冝", "便宜")
+	textTmp = strings.ReplaceAll(textTmp, "消售", "销售")
+	textTmp = strings.ReplaceAll(textTmp, "復挃", "复制")
+	textTmp = strings.ReplaceAll(textTmp, "津帖", "津贴")
+	textTmp = strings.ReplaceAll(textTmp, "厍存", "库存")
+	textTmp = strings.ReplaceAll(textTmp, "妙杀", "秒杀")
+	textTmp = strings.ReplaceAll(textTmp, "欲售", "预售")
+	textTmp = strings.ReplaceAll(textTmp, "钉金", "定金")
+	textTmp = strings.ReplaceAll(textTmp, "订金", "定金")
+	textTmp = strings.ReplaceAll(textTmp, "够物", "购物")
+	textTmp = strings.ReplaceAll(textTmp, "优慧", "优惠")
+	textTmp = strings.ReplaceAll(textTmp, "郵费", "运费")
+	textTmp = strings.ReplaceAll(textTmp, "郵", "邮")
+	textTmp = strings.ReplaceAll(textTmp, "航旗", "旗舰")
+
+	textTmp = strings.ReplaceAll(textTmp, "活费", "话费")
+	textTmp = strings.ReplaceAll(textTmp, "慢冲", "慢充")
+	textTmp = strings.ReplaceAll(textTmp, "荭苞", "红包")
+	textTmp = strings.ReplaceAll(textTmp, "虹包", "红包")
+	textTmp = strings.ReplaceAll(textTmp, "兔費", "免费")
+
+	textTmp = strings.ReplaceAll(textTmp, "亰", "京")
+	textTmp = strings.ReplaceAll(textTmp, "荟", "会")
+	textTmp = strings.ReplaceAll(textTmp, "桃包", "淘宝")
+	textTmp = strings.ReplaceAll(textTmp, "喵抄", "猫超")
+	textTmp = strings.ReplaceAll(textTmp, "喵", "猫")
 
 	if len(textTmp) > 0 {
 		retText = textTmp
